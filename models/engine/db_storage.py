0a186ff64a3d64b9a4b402043280f92f75f48f5a
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-27 02:13:52 +0100

    -m

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,4 +17,50 @@ class DBStorage:
         user = os.getenv('HBNB_MYSQL_USER')
         pwd = os.getenv('HBNB_MYSQL_PWD')
         host = os.getenv('HBNB_MYSQL_HOST')
-        db = os
+        db = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}', pool_pre_ping=True)
+
+        # Drop all tables if the environment is set to 'test'
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def reload(self):
+        """Recreate all tables in the database and establish the session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
+
+    def all(self, cls=None):
+        """Query on the current database session all objects of a given class.
+        If cls=None, query all types of objects."""
+        all_objs = {}
+        if cls is None:
+            classes = [State, City]  # List all classes to query here
+            for cls in classes:
+                objs = self.__session.query(cls).all()
+                for obj in objs:
+                    key = f'{obj.__class__.__name__}.{obj.id}'
+                    all_objs[key] = obj
+        else:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = f'{cls.__name__}.{obj.id}'
+                all_objs[key] = obj
+        return all_objs
+
+    def new(self, obj):
+        """Add the object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session."""
+        if obj:
+            self.__session.delete(obj)
+
+    def close(self):
+        """Dispose the session."""
+        self.__session.close()
