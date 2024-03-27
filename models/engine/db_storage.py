083a544c405e630f4ff55f4b4f652c1680c79ffe
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-27 01:24:48 +0100

    task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(
+            f'mysql+mysqldb://{user}:{pwd}@{host}/{db}')
