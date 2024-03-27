8f408ee5307ae7b6e0bb8b5f5e698b3f76beec38
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-27 01:37:36 +0100

    task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""Defines the DBStorage class."""
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+from models.base_model import Base
+from models.city import City
+from models.state import State
 
 class DBStorage:
+    """Represents a database storage engine."""
     __engine = None
     __session = None
 
     def __init__(self):
+        """Initializes a new DBStorage instance."""
         user = os.getenv('HBNB_MYSQL_USER')
         pwd = os.getenv('HBNB_MYSQL_PWD')
         host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
-        self.__engine = create_engine(
-            f'mysql+mysqldb://{user}:{pwd}@{host}/{db}')
+        db = os
