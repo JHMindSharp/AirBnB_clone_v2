97eb8939a5b72e625989ff5ef8eebd9c92302232
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-29 19:02:51 +0100

    -m

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the Amenity class."""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents an amenity for a MySQL database."""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
