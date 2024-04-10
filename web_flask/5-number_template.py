f11ca68505f18f6d819a5b8ce84aa087e6f323a1
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-10 11:47:29 +0200

    task 5 maj

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,37 +1,57 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Starts a Flask web application with specified routes.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/: Displays 'Hello HBNB!'
+/hbnb: Displays 'HBNB'
+/c/<text>: Displays 'C ' followed by the value of
+the text variable (replace underscore _ symbols with a space).
+/python/(<text>): Displays 'Python ', followed by
+the value of the text variable (replace underscore _ symbols with a space).
+The default value of text is “is cool”.
+/number/<n>: Display 'n is a number' only if n is an integer.
+/number_template/<n>: Display a HTML page only if n is an integer:
+'H1' tag: “Number: n” inside the tag 'BODY'.
+"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!' on the home page."""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    return "C {}".format(text.replace("_", " "))
+    """Displays 'C ' followed by the value of the text variable."""
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    return "Python {}".format(text.replace("_", " "))
+    """Displays 'Python ', followed by the value of the text variable."""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
+    """Display 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """Display a HTML page only if n is an integer."""
     return render_template('5-number.html', n=n)
 
 
